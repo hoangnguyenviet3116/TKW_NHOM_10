32c9e82d75efaad3d6ead3e570708967be6cfef8
Nguyen Phuong Thao <thaophuongg206@gmail.com>
2026-06-07 20:21:53 +0700

    giao dien mobile

--- Trangchitiet/chitiet.js
+++ Trangchitiet/chitiet.js
@@ -963,3 +963,19 @@ function getAllImages() {
 
     return images;
 }
+const menuBtn = document.querySelector('.mobile-menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+
+    const icon = menuBtn.querySelector('i');
+
+    if(menu.classList.contains('active')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    }else{
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+});
